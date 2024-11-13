b875af7a0d31ed1c27365ac58ba26d7c540de90c
Dave Santivañez <davesantivanez@MacBook-Pro-de-Dave.local>
2024-11-12 21:45:38 -0500

    avance lab12 Dave Santivañez

---
+++ APIDemoB/Models/Grade.cs
@@ -0,0 +1,12 @@
+﻿namespace APIDemoB.Models
+{
+    public class Grade
+    {
+
+        public int GradeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; } = true;
+
+    }
+}
